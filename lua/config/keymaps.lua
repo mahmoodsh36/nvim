a2747c65222be9b3d820cc864c5546fb5aa04417
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-07-12 18:00:10 +0300

    reinitiate neovim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- https://www.reddit.com/r/neovim/comments/wg8zin/best_way_to_remap_keys_using_an_initlua/
+local function map(mode, lhs, rhs, opts)
+	local options = { noremap = true, silent = true }
+	if opts then
+		if opts.desc then
+			opts.desc = "keymaps.lua: " .. opts.desc
+		end
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.keymap.set(mode, lhs, rhs, options)
+end
+
+map("n", "s", ":w<Enter>", { desc = "quicker key to save file" })
+map("n", "r", "<C-r>", { desc = "quicker key to save file" })
