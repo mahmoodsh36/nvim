b000462fd66a8bc00f118273f8ab19073f9520a3
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-10-28 16:54:31 +0200

    update, add another theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -505,4 +505,14 @@ return {
       vim.cmd("colorscheme miasma")
     end,
   },
+
+  {
+    'comfysage/evergarden',
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    opts = {
+      transparent_background = true,
+      contrast_dark = 'medium', -- 'hard'|'medium'|'soft'
+      overrides = { }, -- add custom overrides
+    }
+  },
 }
