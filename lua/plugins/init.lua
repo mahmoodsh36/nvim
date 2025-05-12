d6efc3c9d61ef37915cd5fd03db5e335218ad184
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-05-12 14:46:56 +0300

    disable incline, add clipboard syncing over ssh script, update packages

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -516,12 +516,12 @@ return {
     }
   },
 
-  {
-      'b0o/incline.nvim',
-      config = function()
-          require('incline').setup()
-      end,
-      -- Optional: Lazy load Incline
-      event = 'VeryLazy',
-  },
+  -- {
+  --     'b0o/incline.nvim',
+  --     config = function()
+  --         require('incline').setup()
+  --     end,
+  --     -- Optional: Lazy load Incline
+  --     event = 'VeryLazy',
+  -- },
 }
